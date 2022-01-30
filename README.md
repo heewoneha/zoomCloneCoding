50df3bc5accbb53827d17e1cffa4ca5c8ee590e4
heewoneha <jhjmo0719h@naver.com>
2022-01-30 13:47:48 +0000

    Day 4

--- README.md
+++ README.md
@@ -9,13 +9,11 @@ https://nomadcoders.co/c/noom-challenge/lobby
 + 채팅기능 구현
 ![ouput](https://user-images.githubusercontent.com/74031620/150807925-65eb0afc-ba02-4af1-bc84-2012b430848b.png)
 
----------------
 
 #### 2일차
 + 회의 Room 구현 (1)
 ![2일차](https://user-images.githubusercontent.com/74031620/151165020-722eb236-8be2-49fc-b620-6b084958cf4b.png)
 
----------------
 
 #### 3일차
 + 회의 Room 구현 (2)
@@ -24,15 +22,17 @@ https://nomadcoders.co/c/noom-challenge/lobby
 
 ![3일차2](https://user-images.githubusercontent.com/74031620/151685942-f4daad4c-9c28-451f-8d37-dd1f55d3d2e8.png)
 
----------------
 
 #### 4일차
 + 이전 코드 지우고 Room 카메라 기능 구현
-+ (추가 예정)
-
----------------
-
-#### 5일차
-+ CSS 개선
 + 하나의 Room 안에 카메라, 채팅 기능 합치기
-+ (추가 예정)
++ CSS 개선
+![4일차](https://user-images.githubusercontent.com/74031620/151702383-acf33aab-c73c-4368-a93e-654e4afb743c.png)
+
+
+#### 5일차 (예정)
++ CSS 추가 개선
++ 유저 참여시 채팅 박스 안에서 공지
++ 모바일 기기에서 참여할 때 카메라 전환 이후 unmute 풀림 문제 해결
++ 상대방 나갈 때 완전하게 연결 끊기
++ 현재 열린 room 목록 메인 화면에 추가
--- src/server.js
+++ src/server.js
@@ -1,68 +1,38 @@
-import http from "http"; //npm run dev
-import { Server } from "socket.io";
-import { instrument } from '@socket.io/admin-ui';
-import express from "express";
+import http from 'http'; //npm run dev
+import SocketIO from 'socket.io';
+import express from 'express';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-
-const httpServer = http.createServer(app); //Http Server
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ['https://admin.socket.io'],
-        credentials: true,
-    },
-});
-
-instrument(wsServer, {
-    auth: false
-});
-
-function publicRooms(){
-    const sids = wsServer.sockets.adapter.sids;
-    const rooms = wsServer.sockets.adapter.rooms;
-    const public_Rooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined){
-            public_Rooms.push(key);
-        }
-    });
-    return public_Rooms;
-}
-
-function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-    socket['nickname'] = 'Anonymous';
-    socket.onAny((event) => {
-        console.log(wsServer.sockets.adapter);
-        console.log(`Socket Event:${event}`);
-    });
-    socket.on('enter_room', (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit('room_change', publicRooms());
-    });
-    socket.on('disconnecting', () => {
-        socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1));
-    });
-    socket.on('disconnect', () => {
-        wsServer.sockets.emit('room_change', publicRooms());
-    });
-    socket.on('new_message', (msg, room, done) => {
-        socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
+  socket.on('join_room', (roomName, nickName, done) => {
+    socket['nickname'] = nickName;
+    done();
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome', socket.nickname);
+  });
+  socket.on('offer', (offer, roomName) => {
+    socket.to(roomName).emit('offer', offer);
+  });
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
+  });
+  socket.on('ice', (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', socket.nickname, msg);
+    done();
+  });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
